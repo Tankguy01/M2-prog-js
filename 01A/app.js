468b1a2a39e65d15bf48d2d484ffa2d5a5155b54
Naël Oostburg <28110@ma-web.nl>
2023-11-21 12:46:44 +0100

    app

--- 01A/app.js
+++ 01A/app.js
@@ -7,3 +7,19 @@ class App
 }
 let app = new App();
 app.runApplication();
+let variabelwaarde = 100
+let appNaam = " Stars" 
+let versienummer = ' 0.91' 
+let versiedatum= " 21-11-23"
+let autheur = " Nael" 
+let copyright = " Nael Stars"
+let distributeur = " Negative"
+let darkmode = true
+console.log("variabelwaarde" + variabelwaarde)
+console.log("appNaam" + appNaam)
+console.log("versienummer" + versienummer)
+console.log("versiedatum" + versiedatum)
+console.log("autheur" + autheur)
+console.log("copyright" + copyright)
+console.log("distributeur" + distributeur)
+console.log(darkmode)
