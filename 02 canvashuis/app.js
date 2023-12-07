2592b6f07fc0b5ac702c41813a702cab143ade69
Naël Oostburg <28110@ma-web.nl>
2023-12-07 12:33:54 +0100

    canvashuis02

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -5,6 +5,10 @@ class App
         console.log("hello world!")
         let canvas = document.getElementById("canvasId");
         let g = canvas.getContext("2d");
+        g.fillStyle = "#4D94D1";
+        g.fillRect(0,0,canvas.width,canvas.height);
+
+        g.fillStyle = "#544F4F"
         g.fillRect(0,0,10,10);
         console.log(canvas);
     }
