1f28a6bd3337df005adef7af45817c960db0cc98
Naël Oostburg <28110@ma-web.nl>
2024-01-11 12:41:07 +0100

    03 ifelse

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -3,14 +3,23 @@ class App
     runApplication()
     {
         console.log("hello world!")
-        //
-        //
-        //
+        
+
+
+        let title = document.getElementById("newstitle")
+        console.log(title);
+
+        let random = math.random();
+        console.log(random);
+        if(random < 0.2);
+        {
+            title.style.backgroundColor = "#FF0000";
+        }
     }
 }
 let app = new App();
 app.runApplication();
-let title = document.getElementById("newstitle")
+
 
 
 
