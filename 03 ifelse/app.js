904945fa87d111467db99d8a504b2391c31f6b95
Naël Oostburg <28110@ma-web.nl>
2024-01-16 12:12:00 +0100

    03 ifelse

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -3,7 +3,6 @@ class App
     runApplication()
     {
         console.log("hello world!")
-        
 
 
         let title = document.getElementById("newstitle")
@@ -15,7 +14,33 @@ class App
         {
             title.style.backgroundColor = "#FF0000";
         } else {
-            title.style.backgroundColor = "#0000FF"
+            title.style.backgroundColor = "#0000FF";
+        }
+
+
+        let newsitem1 = document.getElementsByClassName("gamenews")[0];
+        console.log(newsitem1);
+
+        let kleur = Math.random();
+        console.log(kleur);
+        if(kleur < 0.2)
+        {
+            newsitem1.style.backgroundColor = "#FFA500";
+        } else {
+            newsitem1.style.backgroundColor = "#FFFF00";
+        }
+
+
+         let newsitem2 = document.getElementsByClassName("gamenews")[1];
+        console.log(newsitem2);
+        
+        let getal = Math.random();
+        console.log(getal);
+        if(getal < 0.2)
+        {
+            newsitem2.style.backgroundColor = "##32CD32";
+        } else {
+            newsitem2.style.backgroundColor = "#5B2AC3";
         }
     }
 }
