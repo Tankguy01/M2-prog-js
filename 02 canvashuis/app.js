89c4e77dc1c0d1e2cdf2f28803a31b6894cc1826
Naël Oostburg <28110@ma-web.nl>
2024-01-16 12:04:39 +0100

    canvashuis

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -5,12 +5,44 @@ class App
         console.log("hello world!")
         let canvas = document.getElementById("canvasId");
         let g = canvas.getContext("2d");
-        g.fillStyle = "#4D94D1";
-        g.fillRect(0,0,canvas.width,canvas.height);
+        g.beginPath()
+       g.fillStyle = "yellow";
+       g.moveTo(70,20);
+       g.lineTo(60,40);
+       g.lineTo(80,30);
+       g.closePath();
+       g.stroke();
+       g.fill();
 
-        g.fillStyle = "#544F4F"
-        g.fillRect(0,0,10,10);
-        console.log(canvas);
+       g.beginPath()
+       g.fillStyle = "yellow";
+       g.moveTo(30,10);
+       g.lineTo(70,20);
+       g.lineTo(60,40);
+       g.lineTo(20,30);
+       g.closePath();
+       g.stroke();
+       g.fill();
+       
+       g.beginPath()
+       g.fillStyle = "yellow";
+       g.moveTo(20,30);
+       g.lineTo(60,40);
+       g.lineTo(60,60);
+       g.lineTo(20,50);
+       g.closePath();
+       g.stroke();
+       g.fill();
+
+       g.beginPath()
+       g.fillStyle = "yellow";
+       g.moveTo(60,40);
+       g.lineTo(80,30);
+       g.lineTo(80,50);
+       g.lineTo(60,60);
+       g.closePath();
+       g.stroke();
+       g.fill();
     }
 }
 
