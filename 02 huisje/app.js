1389f1037b2fb8cb9e5546b42da6160c45f2f86d
Naël Oostburg <28110@ma-web.nl>
2024-01-16 12:11:02 +0100

    02 huisje

---
+++ 02 huisje/app.js
@@ -0,0 +1,50 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!")
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        g.beginPath()
+       g.fillStyle = "gray";
+       g.moveTo(70,20);
+       g.lineTo(60,40);
+       g.lineTo(80,30);
+       g.closePath();
+       g.stroke();
+       g.fill();
+
+       g.beginPath()
+       g.fillStyle = "red";
+       g.moveTo(30,10);
+       g.lineTo(70,20);
+       g.lineTo(60,40);
+       g.lineTo(20,30);
+       g.closePath();
+       g.stroke();
+       g.fill();
+       
+       g.beginPath()
+       g.fillStyle = "lightgray";
+       g.moveTo(20,30);
+       g.lineTo(60,40);
+       g.lineTo(60,60);
+       g.lineTo(20,50);
+       g.closePath();
+       g.stroke();
+       g.fill();
+
+       g.beginPath()
+       g.fillStyle = "gray";
+       g.moveTo(60,40);
+       g.lineTo(80,30);
+       g.lineTo(80,50);
+       g.lineTo(60,60);
+       g.closePath();
+       g.stroke();
+       g.fill();
+    }
+}
+
+let app = new App();
+app.runApplication();
